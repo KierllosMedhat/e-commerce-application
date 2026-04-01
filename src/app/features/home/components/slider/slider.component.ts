1b9a22729877c0ac04a84ead2ef72d689ce31b61
KierllosMedhat <kierllosmedhat@outlook.com>
2026-04-02 00:48:12 +0200

    feat: add swiper carousel component

--- src/app/features/home/components/slider/slider.component.ts
+++ src/app/features/home/components/slider/slider.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { register } from 'swiper/element/bundle';
 
 @Component({
   selector: 'app-slider',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.css',
 })
-export class SliderComponent {}
+export class SliderComponent implements OnInit{
+  ngOnInit(): void {
+    register();
+  }
+}
